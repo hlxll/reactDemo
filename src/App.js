cc902262c45040803ee1e64bd4bf7fe6a9a7d4cc
黄林19 <huanglin19@hikvision.com>
2020-11-19 18:15:02 +0800

    'text'

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import {HashRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
 import Login from './pages/login/login'
 import Admin from './pages/admin/admin'
 
-
+//如果要把redux和react连接，要使用provide，把redux传入react项目中
+//在组件中需要使用react-redux的connect，想高阶组件一样，连接redux和react
 export default class App extends Component{
     render(){
         return(
