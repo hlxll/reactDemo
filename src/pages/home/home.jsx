50d27c68822be1b7c0bfa45b18a68303dd946742
黄林19 <huanglin19@hikvision.com>
2020-11-24 19:14:45 +0800

    'fragment'

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,8 +4,11 @@ class Home extends Component{
   render(){
     return(
       <div className="home">
-          refs转发，将ref传入子组件，但是ref不会随props传递，在子组件中使用React.forwardRef((props, ref)接收两个参数，获取props和ref
-          对于复用性很大的组件，需要控制子组件时候，可以考虑refs转发
+        <p>refs转发</p>
+        refs转发，将ref传入子组件，但是ref不会随props传递，在子组件中使用React.forwardRef((props, ref)接收两个参数，获取props和ref
+        对于复用性很大的组件，需要控制子组件时候，可以考虑refs转发
+        <p>fragment</p>
+        使用React.fragment包裹组件，不会在外部形成div，比如特定的table内部的tr内部只能有td，就可以用这个
       </div>
     )
   }
