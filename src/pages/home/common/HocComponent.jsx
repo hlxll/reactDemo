988d53b1881f8d8a8b3b238a371e113dbd242fa4
黄林19 <huanglin19@hikvision.com>
2020-11-25 14:33:11 +0800

    '高阶组件'

---
+++ src/pages/home/common/HocComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HocComponent extends Component{
+    render(){
+        return(
+            <div>
+                高阶组件使用
+            </div>
+        )
+    }
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,18 @@
 import React,{ Component } from "react"
 import './home.less'
+import HocComponent from './common/HocComponent'
 class Home extends Component{
+  HocCom(HComponent){
+      return class extends Component {
+        render(){
+          return(
+            <HComponent/>
+          )
+        }
+      }
+    }
   render(){
+    const Hoc = this.HocCom(HocComponent)
     return(
       <div className="home">
         <p>refs转发</p>
@@ -9,6 +20,8 @@ class Home extends Component{
         对于复用性很大的组件，需要控制子组件时候，可以考虑refs转发
         <p>fragment</p>
         使用React.fragment包裹组件，不会在外部形成div，比如特定的table内部的tr内部只能有td，就可以用这个
+        <p>高阶组件</p>
+        <Hoc />
       </div>
     )
   }
