80f46efc2d0f5b434586b9d638852839047163a3
黄林19 <huanglin19@hikvision.com>
2020-12-11 13:06:33 +0800

    'update'

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -8,21 +8,26 @@ import  'echarts/lib/chart/bar';
 // 引入提示框和标题组件
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/title';
-import { Carousel } from 'antd';
 class Home extends Component{
+  constructor(prop){
+    super(prop)
+    this.state = {
+      detailData:''
+    }
+  }
   componentDidMount(){
-    var data1 = [2000,3000,5500,7000,8000,12000,20000]
-    var data2 = [1500,3000,4500,6000,8000,10000,15000]
-    var data3 = [1000,2000,2500,4000,6000,7000,8000]
-    var myChart1 = echarts.init(document.getElementById('oneEcharts'))
-    var myChart2 = echarts.init(document.getElementById('twoEcharts'))
-    var myChart3 = echarts.init(document.getElementById('threeEcharts'))
+    var data1 = [[20,30,55,70,80,120,200],[15,30,45,60,80,100,150],[10,20,25,40,60,70,80]]
+    var data2 = [[20,30,55,70,80,120,200],[15,30,45,60,80,100,150],[10,20,25,40,60,70,80]]
+    var data3 = [[20,30,55,70,80,120,200],[15,30,45,60,80,100,150],[10,20,25,40,60,70,80]]
+    var myChart1 = echarts.init(document.getElementById('oneEchart'))
+    var myChart2 = echarts.init(document.getElementById('twoEchart'))
+    var myChart3 = echarts.init(document.getElementById('threeEchart'))
     myChart1.setOption({
       title:{
-        text:'商品数量趋势图'
+        text:'种类趋势图'
       },
       legend:{
-        data:['OFO','摩拜','小蓝']
+        data:['书本','食物','家具']
       },
       tooltip:{   //展示数据
         trigger:'axis'
@@ -35,26 +40,26 @@ class Home extends Component{
       },
       series:[
         {
-          name:'OFO',
+          name:'书本',
           type:'bar',
-          data:data1
+          data:data1[0]
         },{
-          name:'摩拜',
+          name:'食物',
           type:'bar',
-          data:data2
+          data:data1[1]
         },{
-          name:'小蓝',
+          name:'家具',
           type:'bar',
-          data:data3
+          data:data1[2]
         }
       ]
   })
     myChart2.setOption({
       title:{
-        text:'商品数量趋势图'
+        text:'数量趋势图'
       },
       legend:{
-        data:['OFO','摩拜','小蓝']
+        data:['书本','食物','家具']
       },
       tooltip:{   //展示数据
         trigger:'axis'
@@ -67,26 +72,26 @@ class Home extends Component{
       },
       series:[
         {
-          name:'OFO',
+          name:'书本',
           type:'bar',
-          data:[2000,3000,5500,7000,8000,12000,20000]
+          data:data2[0]
         },{
-          name:'摩拜',
+          name:'食物',
           type:'bar',
-          data:[1500,3000,4500,6000,8000,10000,15000]
+          data:data2[1]
         },{
-          name:'小蓝',
+          name:'家具',
           type:'bar',
-          data:[1000,2000,2500,4000,6000,7000,8000]
+          data:data2[2]
         }
       ]
   })
     myChart3.setOption({
       title:{
-        text:'商品数量趋势图'
+        text:'价格趋势图'
       },
       legend:{
-        data:['OFO','摩拜','小蓝']
+        data:['书本','食物','家具']
       },
       tooltip:{   //展示数据
         trigger:'axis'
@@ -99,17 +104,17 @@ class Home extends Component{
       },
       series:[
         {
-          name:'OFO',
+          name:'书本',
           type:'bar',
-          data:[2000,3000,5500,7000,8000,12000,20000]
+          data:data3[0]
         },{
-          name:'摩拜',
+          name:'食物',
           type:'bar',
-          data:[1500,3000,4500,6000,8000,10000,15000]
+          data:data3[1]
         },{
-          name:'小蓝',
+          name:'家具',
           type:'bar',
-          data:[1000,2000,2500,4000,6000,7000,8000]
+          data:data3[2]
         }
       ]
     })
@@ -122,49 +127,52 @@ class Home extends Component{
           )
         }
       }
-    }
+  }
+  openDetil(item){
+    // this.setState({
+    //   detailData: ''
+    // })
+  }
   render(){
     const Hoc = this.HocCom(HocComponent)
     return(
       <div className="home">
         <div className="threeEcharts">
-          <div id="oneEcharts" style={{ width: 400, height: 400 }}></div>
-          <div id="twoEcharts" style={{ width: 400, height: 400 }}></div>
-          <div id="threeEcharts" style={{ width: 400, height: 400 }}></div>
+          <div id="oneEchart" style={{ width: 400, height: 400 }}></div>
+          <div id="twoEchart" style={{ width: 400, height: 400 }}></div>
+          <div id="threeEchart" style={{ width: 400, height: 400 }}></div>
         </div>
         <div className="homeMain">
-        <Carousel autoplay>
-          <div>
+          <div className="homeMainChild">
             <div className="autoDiv">
-            <div className="homeImg"></div>
-              <div>
-                <p>huanglin</p>
-                <p>jiesaoizixsa</p>
-                <p>200</p>
-              </div>
+            <div className="homeImg1"></div>
+            <div className="Rightmain">
+              <p>书本</p>
+              <p className="link" onClick={this.openDetil('1')}>书本的详细信息</p>
+              <p>作者：黄林</p>
+            </div>
             </div>
           </div>
-          <div className="">
+          <div className="homeMainChild">
             <div className="autoDiv">
-            <div className="homeImg"></div>
-              <div>
-                <p>huanglin</p>
-                <p>jiesaoizixsa</p>
-                <p>200</p>
-              </div>
+            <div className="homeImg2"></div>
+            <div className="Rightmain">
+              <p>食物</p>
+              <p className="link" onClick={this.openDetil('2')}>食物的详细信息</p>
+              <p>供给：钉钉买菜</p>
+            </div>
             </div>
           </div>
-          <div className="">
+          <div className="homeMainChild">
             <div className="autoDiv">
-            <div className="homeImg"></div>
-              <div>
-                <p>huanglin</p>
-                <p>jiesaoizixsa</p>
-                <p>200</p>
-              </div>
+            <div className="homeImg3"></div>
+            <div className="Rightmain">
+              <p>家具</p>
+              <p className="link" onClick={this.openDetil('3')}>家具的详细信息</p>
+              <p>供给：宜家</p>
+            </div>
             </div>
           </div>
-        </Carousel>
         </div>
         <Hoc />
       </div>
